34034bf1c1d536ca67f84d1456fbec53cf01481a
Valentin <valentin.foucher@free.fr>
2023-05-26 14:12:04 +0200

    refactor: extract business logic from main program

--- cmd/main.go
+++ cmd/main.go
@@ -2,42 +2,23 @@ package main
 
 import (
 	"fmt"
-	"sort"
 
 	"github.com/Valentin-Foucher/rss-feed-aggregator/pkg/rss"
 )
 
-func sortItems(itemsCollection *rss.ItemsCollection) {
-	sort.Slice(itemsCollection.Items, func(i, j int) bool {
-		return itemsCollection.Items[i].ParsedPublishedDate().After(*itemsCollection.Items[j].ParsedPublishedDate())
-	})
-}
-
-func fromFeeds(feedUrls []string) error {
-	// aggregate
-	itemsCollection, err := rss.GetItemsFromFeeds(feedUrls)
-	if err != nil {
-		return err
-	}
-
-	// sort historically
-	sortItems(itemsCollection)
-
-	iter := itemsCollection.CreateIterator()
-	for iter.HasNext() {
-		itemsWindow := iter.Next()
-		for _, item := range itemsWindow {
-			if item != nil {
-				fmt.Println(item.Title())
+func main() {
+	if iter, err := rss.FromFeeds([]string{"https://feeds.simplecast.com/54nAGcIl", "https://www.dailymail.co.uk/sciencetech/index.rss"}); err == nil {
+		for iter.HasNext() {
+			itemsWindow := iter.Next()
+			for _, item := range itemsWindow {
+				if item != nil {
+					fmt.Println(item.Title())
+				}
 			}
 		}
+	} else {
+		fmt.Println(err)
 	}
-
-	return nil
-}
-
-func main() {
-	fromFeeds([]string{"https://feeds.simplecast.com/54nAGcIl", "https://www.dailymail.co.uk/sciencetech/index.rss"})
 
 	// reader := strings.NewReader(feed.Items[0].Content)
 	// browser.OpenReader(reader)
---
+++ pkg/rss/aggregator.go
@@ -0,0 +1,25 @@
+package rss
+
+import (
+	"sort"
+)
+
+func sortItems(itemsCollection *ItemsCollection) {
+	sort.Slice(itemsCollection.Items, func(i, j int) bool {
+		return itemsCollection.Items[i].ParsedPublishedDate().After(*itemsCollection.Items[j].ParsedPublishedDate())
+	})
+}
+
+func FromFeeds(feedUrls []string) (IItemsIterator, error) {
+	// aggregate
+	itemsCollection, err := GetItemsFromFeeds(feedUrls)
+	if err != nil {
+		return nil, err
+	}
+
+	// sort historically
+	sortItems(itemsCollection)
+
+	iter := itemsCollection.CreateIterator()
+	return iter, nil
+}
