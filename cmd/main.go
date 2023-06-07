77e5aa30f54f57d6275044d5934d3a00f2cab2fa
Valentin <valentin.foucher@free.fr>
2023-06-07 14:35:35 +0200

    feat: make iterator go both ways

--- cmd/main.go
+++ cmd/main.go
@@ -2,24 +2,10 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/Valentin-Foucher/rss-feed-aggregator/pkg/rss"
 )
 
 func main() {
-	if iter, err := rss.GetItemsIteratorFromFeeds([]string{"https://feeds.simplecast.com/54nAGcIl", "https://www.dailymail.co.uk/sciencetech/index.rss"}); err == nil {
-		for iter.HasNext() {
-			itemsWindow := iter.Next()
-			for _, item := range itemsWindow {
-				if item != nil {
-					fmt.Println(item.Title())
-				}
-			}
-		}
-	} else {
-		fmt.Println(err)
-	}
-
-	// reader := strings.NewReader(feed.Items[0].Content)
-	// browser.OpenReader(reader)
+	var x []int
+	x = []int{1, 2, 3, 4, 5}
+	fmt.Print(x[2:10])
 }
--- pkg/rss/iterator.go
+++ pkg/rss/iterator.go
@@ -2,7 +2,9 @@ package rss
 
 type IItemsIterator interface {
 	HasNext() bool
+	HasPrevious() bool
 	Next() []IItem
+	Previous() []IItem
 }
 
 type ItemsIterator struct {
@@ -21,9 +23,33 @@ func (i *ItemsIterator) HasNext() bool {
 }
 func (i *ItemsIterator) Next() []IItem {
 	if i.HasNext() {
-		items := i.items[i.index : i.index+i.window]
-		i.index += i.window
-		return items
+		last := i.index + i.window
+		if last > len(i.items)-1 {
+			last = len(i.items) - 1
+		}
+		result := i.items[i.index:last]
+		i.index = last
+		return result
+	}
+	return nil
+}
+
+func (i *ItemsIterator) HasPrevious() bool {
+	if i.index > 0 {
+		return true
+	}
+	return false
+
+}
+func (i *ItemsIterator) Previous() []IItem {
+	if i.HasPrevious() {
+		first := i.index - i.window
+		if first < 0 {
+			first = 0
+		}
+		result := i.items[first:i.index]
+		i.index = first
+		return result
 	}
 	return nil
 }
